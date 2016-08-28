e4772419eb9523c4795ad53585941ced90229c19
Kyle Keebler <kylekeebler@gmail.com>
2016-08-28 16:05:00 -0400

    Brick Update and Test scene

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -11,95 +11,134 @@ public class Brick : MonoBehaviour
 	public bool groud;
 	public bool doNothing = false;
 	public float distancey;
+	public bool groundOn;
+	public float knockBackForce;
+	public bool floating;
+	public RaycastHit rayInfo;
+
+	public Vector3 originaScale;
+	public Vector3 maxScale;
+
+	public float vel;
 	// Use this for initialization
 	void Start ()
 	{
 		deathTime = timeOutTime;
 		doNothing = false;
+		originaScale = transform.localScale;
+		maxScale = transform.localScale;
+		maxScale.x *= 2;
+		maxScale.y *= 2;
+		maxScale.z *= 2;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		if (Floating())// && firstAttached)
+		if (hit && firstAttached)
 		{
-			gameObject.transform.DetachChildren();
-			master = null;
-
 			gameObject.GetComponent<Rigidbody>().isKinematic = false;
-			gameObject.transform.parent = null;
 		}
+		//if (transform.localScale.y > originaScale.y || transform.localScale.x > originaScale.x || transform.localScale.z > originaScale.z)
+		//{
+		//	hit = true;
+		//	firstAttached = true;
+		//	master = null;
+		//	transform.parent = null;
+		//}
+		if (transform.localScale.y > maxScale.y || transform.localScale.x > maxScale.x || transform.localScale.z > maxScale.z)
+		{
+			Destroy(gameObject);
+		}
+		groundOn = IsGroundOrOnObject();
+		vel = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+		floating = Floating();
+		//if (Floating())
+		//{
+		//	firstAttached = true;
+		//	gameObject.GetComponent<Rigidbody>().isKinematic = false;
+		//	gameObject.GetComponent<Rigidbody>().useGravity = true;
+		//}
+
 		if (transform.parent != null)
 		{
 			distancey = Vector3.Distance(transform.parent.transform.position, transform.position);//Mathf.Round(transform.parent.transform.position.y - transform.position.y);
 		}
-		groud = IsGrounded();
-		if (IsGrounded())
+
+		if (IsGroundOrOnObject() && firstAttached && hit && gameObject.GetComponent<Rigidbody>().velocity.magnitude < 0.1f)
 		{
-			if (!hit)
-			{
-				//doNothing = true;
-				gameObject.GetComponent<Rigidbody>().isKinematic = true;
-			}
+			gameObject.GetComponent<Rigidbody>().isKinematic = true;
+			gameObject.GetComponent<Rigidbody>().useGravity = true;
+			master = null;
 		}
 		else
 		{
-			
-			if (master != null)
+			if (Floating())// && firstAttached)
 			{
-
-				if (!master.GetComponent<Rigidbody>().isKinematic)
-				{
-					gameObject.transform.DetachChildren();
-					master = null;
-
-					gameObject.GetComponent<Rigidbody>().isKinematic = false;
-					gameObject.transform.parent = null;
-					//Debug.Log("Seven");
-				}
-				else if (firstAttached && Mathf.Abs(distancey) > 10)
-				{
-					gameObject.transform.DetachChildren();
-					master = null;
-
-					gameObject.GetComponent<Rigidbody>().isKinematic = false;
-					gameObject.transform.parent = null;
-					//Debug.Log("Sixth");
-				}
-				//Debug.Log("NotNUll");
-			}
-			else if (firstAttached && transform.parent == null)
-			{
-				gameObject.transform.parent = null;
-				gameObject.GetComponent<Rigidbody>().isKinematic = false;
 				gameObject.transform.DetachChildren();
-				hit = true;
-				//Debug.Log("Fifth"+ gameObject.transform.position);
+				master = null;
+
+				gameObject.GetComponent<Rigidbody>().isKinematic = false;
+				gameObject.transform.parent = null;
 			}
-			if (gameObject.GetComponent<Rigidbody>().isKinematic)
+			
+			groud = IsGrounded();
+			if (IsGrounded())
 			{
-				if (GetComponent<Rigidbody>().velocity.magnitude > 0.01f && !hit && firstAttached)
+
+				//doNothing = true;
+				gameObject.GetComponent<Rigidbody>().isKinematic = true;
+				master = null;
+			}
+			else
+			{
+
+				if (master != null)
 				{
+
+					if (!master.GetComponent<Rigidbody>().isKinematic)
+					{
+					//	gameObject.transform.DetachChildren();
+					//	master = null;
+
+					//	gameObject.GetComponent<Rigidbody>().isKinematic = false;
+					//	gameObject.transform.parent = null;
+					//	Debug.Log("Seven");
+					}
+					else if (firstAttached && Mathf.Abs(distancey) > 4 )
+					{
+						gameObject.transform.DetachChildren();
+						master = null;
+
+						gameObject.GetComponent<Rigidbody>().isKinematic = false;
+						gameObject.transform.parent = null;
+						//Debug.Log("Sixth");
+					}
+					////Debug.Log("NotNUll");
+				}
+				else if (firstAttached && transform.parent == null && Floating())
+				{
+					master = null;
+					gameObject.transform.parent = null;
+					gameObject.GetComponent<Rigidbody>().isKinematic = false;
 					gameObject.transform.DetachChildren();
 					hit = true;
-
-					master = null;
-					gameObject.GetComponent<Rigidbody>().isKinematic = false;
-					gameObject.transform.parent = null;
-					//Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
-					//Debug.Log("Fourth");
+					//Debug.Log("Fifth"+ gameObject.transform.position);
 				}
+				if (gameObject.GetComponent<Rigidbody>().isKinematic)
+				{
+					if (GetComponent<Rigidbody>().velocity.magnitude > 0.01f && !hit && firstAttached)
+					{
+						gameObject.transform.DetachChildren();
+						hit = true;
 
-				//if (firstAttached && master == null && gameObject.transform.childCount < 2 && !IsGrounded())
-				//{
-				//	deathTime -= Time.deltaTime;
-				//	if (deathTime < 0)
-				//	{
-				//		gameObject.GetComponent<Rigidbody>().isKinematic = false;
-				//		gameObject.transform.parent = null;
-				//		Debug.Log("Third");
-				//	}
-				//}
+						master = null;
+						gameObject.GetComponent<Rigidbody>().isKinematic = false;
+						gameObject.transform.parent = null;
+						////Debug.Log(GetComponent<Rigidbody>().velocity.magnitude);
+						Debug.Log("Fourth");
+					}
+				}
 			}
 		}
 	}
@@ -150,12 +189,17 @@ public class Brick : MonoBehaviour
 		{
 			doNothing = false;
 			gameObject.transform.DetachChildren();
-			hit = true;
-
 			master = null;
 			gameObject.GetComponent<Rigidbody>().isKinematic = false;
 			gameObject.transform.parent = null;
-			//gameObject.GetComponent<Rigidbody>().AddForce(collision.gameObject.transform.forward * 10000);
+			if (!hit)
+			{
+				gameObject.GetComponent<Rigidbody>().AddForce(collision.gameObject.GetComponent<Rigidbody>().velocity * knockBackForce);
+			}
+			hit = true;
+
+			
+			
 			//Debug.Log("Second");
 		}
 
@@ -173,16 +217,52 @@ public class Brick : MonoBehaviour
 		}
 	}
 
+	void OnTriggerStay(Collider trigger)
+	{
+		if (trigger.gameObject.tag.Equals("Buffer", System.StringComparison.OrdinalIgnoreCase))
+		{
+			gameObject.GetComponent<Rigidbody>().isKinematic = false;
+			gameObject.transform.parent = null;
+			//Debug.Log("Second");
+		}
+	}
+
 	float GroundDistance;
 	bool IsGrounded()
 	{
 		RaycastHit info = new RaycastHit();
-		Physics.Raycast(transform.position,  -Vector3.up, out info, GroundDistance + 0.13f);
+		Physics.Raycast(transform.position,  -Vector3.up, out info, GroundDistance + 0.1f);
 		if (info.transform != null)
 		{
 			if (info.transform.tag.Equals("Ground"))
 			{
 				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return Physics.Raycast(transform.position, -Vector3.up, GroundDistance + 0.1f);
+	}
+
+	bool IsGroundOrOnObject()
+	{
+		RaycastHit info = new RaycastHit();
+		Physics.Raycast(transform.position, -Vector3.up, out info, GroundDistance + 0.13f);
+		rayInfo = info;
+		if (info.transform != null)
+		{
+			if (info.transform.tag.Equals("Ground"))
+			{
+				return true;
+			}
+			else if (info.transform.tag.Equals("Brick"))
+			{
+				if (info.transform.tag.Equals("Brick") && Vector3.Distance(info.transform.position, transform.position) < 0.1f && info.transform.GetComponent<Brick>().IsGrounded())
+				{
+					return true;
+				}
 			}
 			else
 			{
@@ -195,10 +275,10 @@ public class Brick : MonoBehaviour
 	bool Floating()
 	{
 		RaycastHit info = new RaycastHit();
-		Physics.Raycast(transform.position, -Vector3.up, out info, GroundDistance + 1f);
+		Physics.Raycast(transform.position, -Vector3.up, out info, GroundDistance + transform.localScale.x);
 		if (info.transform != null)
 		{
-			if (info.transform.tag.Equals("Brick") && Vector3.Distance(info.transform.position, transform.position) > 1.0f)
+			if (info.transform.tag.Equals("Brick") && Vector3.Distance(info.transform.position, transform.position) > transform.localScale.x)
 			{
 				return true;
 			}
@@ -207,6 +287,7 @@ public class Brick : MonoBehaviour
 				return false;
 			}
 		}
-		return Physics.Raycast(transform.position, -Vector3.up, GroundDistance + 1f);
+		return true;
+		//return Physics.Raycast(transform.position, -Vector3.up, GroundDistance + transform.localScale.x);
 	}
 }
